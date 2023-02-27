ff414b01f5b45df77e392e95c9de6d4536ece144
putrid-nonsense <thebetterclmagnus@gmail.com>
2023-02-27 17:41:47 +0000

    Update GenericPlane.gd

--- scenes/GenericPlane.gd
+++ scenes/GenericPlane.gd
@@ -1,9 +1,9 @@
 extends CharacterBody3D
 
-var 
+#Rotational velocity
+var rotVelocity = Vector3(0.0 ,0.0 ,0.0)
 
 func _physics_process(delta):
-
-	var yaw
-	var pitch
-	var roll = get_axis()
+	var yaw = Input.get_axis("Yaw+","Yaw-")
+	var pitch = Input.get_axis("Pitch+","Pitch-")
+	var roll = Input.get_axis("Roll+","Roll-")
